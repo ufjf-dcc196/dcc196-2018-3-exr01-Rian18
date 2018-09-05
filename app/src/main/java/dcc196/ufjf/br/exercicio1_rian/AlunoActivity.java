a99d4b1067ec53a757d7d9c3c0ff31e079fd7bfb
Rian Alves <rianalves2009.ra@gmail.com>
2018-09-05 20:52:46 -0300

    Alterações nos Activitys

--- app/src/main/java/dcc196/ufjf/br/exercicio1_rian/AlunoActivity.java
+++ app/src/main/java/dcc196/ufjf/br/exercicio1_rian/AlunoActivity.java
@@ -3,7 +3,7 @@ package dcc196.ufjf.br.exercicio1_rian;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Aluno extends AppCompatActivity {
+public class AlunoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/dcc196/ufjf/br/exercicio1_rian/MainActivity.java
+++ app/src/main/java/dcc196/ufjf/br/exercicio1_rian/MainActivity.java
@@ -1,13 +1,35 @@
 package dcc196.ufjf.br.exercicio1_rian;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnAluno;
+    private Button btnServidor;
+    private Button btnExterno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAluno = (Button)findViewById(R.id.btn_ALuno);
+        btnServidor = (Button)findViewById(R.id.btn_Servidor);
+        btnExterno = (Button)findViewById(R.id.btn_Externo);
+
+        btnAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AlunoActivity.class);
+                //intent.putExtra(PESSOA_NOME, edtNome.getText().toString());
+                startActivity(intent);
+                //startActivityForResult(intent, MainActivity.REQUEST_SIAPE);
+            }
+        });;
+
     }
 }
