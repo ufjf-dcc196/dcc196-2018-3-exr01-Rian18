7a2521c6bb8cf85fd3bd342509e3ec718ae6bb66
Rian Alves <rianalves2009.ra@gmail.com>
2018-09-05 20:32:31 -0300

    Criação da Activity Aluno

---
+++ app/src/main/java/dcc196/ufjf/br/exercicio1_rian/Aluno.java
@@ -0,0 +1,13 @@
+package dcc196.ufjf.br.exercicio1_rian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Aluno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aluno);
+    }
+}
